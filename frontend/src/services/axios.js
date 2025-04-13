4d23f2f8afcd319156e17e486cfe8d5262bd1ee2
Herlan Villafer <herdevlanx@gmail.com>
2025-04-12 22:54:32 -0400

    Despliegue funcional Frontend

--- frontend/src/services/axios.js
+++ frontend/src/services/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+// Verifica si la variable se está leyendo correctamente
+console.log("✅ API URL:", import.meta.env.VITE_API_URL);
+
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:4000/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 API.interceptors.request.use((req) => {
